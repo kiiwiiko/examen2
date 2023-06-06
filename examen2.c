f78375e9e99c08d73f4ebcae018c53df828b5e22
Arie Quimi <trr.2002j@gmail.com>
2023-06-06 13:02:51 -0500

    creacion de las funciones

--- examen2.c
+++ examen2.c
@@ -3,8 +3,38 @@
 #include <time.h>
 #include <conio.h>
 
-int main(){
-    
-    getch();
-    return 0;
+#define NUM_ALUMNOS 23
+#define NUM_PROGRESOS 3
+
+// Función para obtener el promedio de calificaciones de un alumno en un progreso
+float obtenerPromedioAlumno(float calificaciones[NUM_PROGRESOS]) {
+    float suma = 0;
+    for (int i = 0; i < NUM_PROGRESOS; i++) {
+        suma += calificaciones[i];
+    }
+    return suma / NUM_PROGRESOS;
+}
+
+// Función para obtener el promedio del grupo de estudiantes en un progreso
+float obtenerPromedioGrupo(float calificacionesGrupo[NUM_ALUMNOS][NUM_PROGRESOS], int progreso) {
+    float suma = 0;
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        suma += calificacionesGrupo[i][progreso];
+    }
+    return suma / NUM_ALUMNOS;
+}
+
+// Función para encontrar al alumno con el mayor promedio de calificación en el semestre
+int encontrarMejorAlumno(float calificacionesGrupo[NUM_ALUMNOS][NUM_PROGRESOS]) {
+    int mejorAlumno = 0;
+    float mejorPromedio = obtenerPromedioAlumno(calificacionesGrupo[0]);
+
+    for (int i = 1; i < NUM_ALUMNOS; i++) {
+        float promedio = obtenerPromedioAlumno(calificacionesGrupo[i]);
+        if (promedio > mejorPromedio) {
+            mejorPromedio = promedio;
+            mejorAlumno = i;
+        }
+    }
+    return mejorAlumno;
 }
