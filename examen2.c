6cd01ed67a6559847a27ffc36aa8d7126cf15956
Arie Quimi <trr.2002j@gmail.com>
2023-06-06 13:19:50 -0500

    usar las funciones

--- examen2.c
+++ examen2.c
@@ -41,6 +41,48 @@ int encontrarMejorAlumno(float calificacionesGrupo[NUM_ALUMNOS][NUM_PROGRESOS]) 
 
 int main(int argc, char const *argv[])
 {
+    float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS];
+
+    // Generar calificaciones aleatorias para los alumnos
+    srand(time(0)); // Semilla para generar números aleatorios
+
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        for (int j = 0; j < NUM_PROGRESOS; j++) {
+            calificaciones[i][j] = rand() % 11; // Generar números aleatorios entre 0 y 11
+        }
+    }
+
+    // Imprimir las calificaciones de los alumnos
+    printf("Calificaciones de los alumnos:\n");
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        printf("Alumno %d:\n", i + 1);
+        for (int j = 0; j < NUM_PROGRESOS; j++) {
+            printf("Progreso %d: %.2f\n", j + 1, calificaciones[i][j]);
+        }
+    }
+
+    // Calcular y mostrar los resultados
+    printf("\nResultados:\n");
+
+    // 1- Obtener el promedio de calificaciones de cada alumno durante el semestre
+    printf("Promedio de calificaciones de cada alumno:\n");
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        float promedio = obtenerPromedioAlumno(calificaciones[i]);
+        printf("Alumno %d: %.2f\n", i + 1, promedio);
+    }
+
+    // 2- La nota promedio del grupo de estudiantes para cada progreso
+    printf("\nNota promedio del grupo para cada progreso:\n");
+    for (int j = 0; j < NUM_PROGRESOS; j++) {
+        float promedioGrupo = obtenerPromedioGrupo(calificaciones, j);
+        printf("Progreso %d: %.2f\n", j + 1, promedioGrupo);
+    }
+
+    // 3- El alumno que obtuvo el mayor promedio de calificación durante el semestre
+    int mejorAlumno = encontrarMejorAlumno(calificaciones);
+    printf("\nAlumno con el mayor promedio de calificación durante el semestre:\n");
+    printf("Alumno %d\n", mejorAlumno + 1);
+
     getch();
     return 0;
 }
